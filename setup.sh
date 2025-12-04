060eee3690d034f107e3e4babf0e18fd7e66e372
Javier Bedoya <javierbedoya@Javiers-MacBook-Air-2.local>
2025-12-04 15:24:42 -0700

    updates to the script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Install Ninja
-curl -o /tmp/ninja.pkg "https://app.ninjarmm.com/agent/installer/fff96fdb-6766-4611-8c80-ac7f24688fc3/8.0.3261/NinjaOne-Agent_fff96fdb-6766-4611-8c80-ac7f24688fc3-LandlordTech-MainOffice-Auto.dmg"
-sudo installer -pkg /tmp/ninja.pkg -target /
+curl -o /tmp/ninja.dmg "https://app.ninjarmm.com/agent/installer/fff96fdb-6766-4611-8c80-ac7f24688fc3/8.0.3261/NinjaOne-Agent_fff96fdb-6766-4611-8c80-ac7f24688fc3-LandlordTech-MainOffice-Auto.dmg"
+hdiutil attach /tmp/ninja.dmg -nobrowse -quiet
+sudo installer -pkg "/Volumes/NinjaRMMAgent/NinjaRMMAgent.pkg" -target /
+hdiutil detach "/Volumes/NinjaRMMAgent" -quiet
 
-echo "Ninja installed - check your dashboard"
+echo "Done - Ninja"
